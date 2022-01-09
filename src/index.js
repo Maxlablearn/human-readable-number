26e0f76bff4729841acc49d36f5e3c881419b820
MaxLabLearn <maxlablearn@gmail.com>
2022-01-09 12:43:49 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    let dict20 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dictTy = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number > 99) {
+        if (number % 100 == 0) {
+            return ('' + dict20[Math.floor(number / 100)] + ' hundred');
+        }
+        return ('' + dict20[Math.floor(number / 100)] + ' hundred ' + toReadable(number % 100));
+    } else {
+        if (number > 19) {
+            if (number % 10 == 0) {
+                return ('' + dictTy[Math.floor(number / 10)]);
+            };
+            return ('' + dictTy[Math.floor(number / 10)]+ ' ' + toReadable(number % 10));
+        } else {
+            return ('' + dict20[number])
+        }
+    }
 }
